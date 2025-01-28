57275f9018760ecede3a094d591d2ece922c92f9
maxsvensbyNTI2C <150040754+MaxSvensby@users.noreply.github.com>
2025-01-28 16:28:35 +0100

    2025-01-28

--- app.rb
+++ app.rb
@@ -4,8 +4,24 @@ require 'sqlite3'
 require 'sinatra/reloader'
 require 'bcrypt'
 
+enable:sessions
+
+before do
+    p "Körs innan alla routes, kolla om man är inloggad"
+end
+
+def connect_db()
+    db = SQLite3::Database.new('db/csgo.db')
+    db.results_as_hash = true
+    return db
+end
+
 get ('/') do
     slim(:home)
+end
+
+get ('/loginpage') do
+    slim(:loginpage)
 end
 
 get ('/inventory') do
