1b1fc76f2db1eeea7856c96c2a24881de52f0a3f
maxsvensbyNTI2C <150040754+MaxSvensby@users.noreply.github.com>
2025-02-04 16:29:28 +0100

    2025-02-04

--- app.rb
+++ app.rb
@@ -24,6 +24,38 @@ get ('/loginpage') do
     slim(:loginpage)
 end
 
+post ('/register') do
+    username = params[:username]
+    password = params[:password]
+    password_confirm = params[:password_confirm]
+    admin = params[:admin]
+
+    if (password == password_confirm)
+        balance = 0
+        password_digest = BCrypt::Password.create(password)
+        db = SQLite3::Database.new('db/csgo.db')
+        db.execute('INSERT INTO users (username,pwdigest,balance,admin) VALUES (?,?,?,?)', [username,password_digest,balance,admin])
+        redirect('/')
+    else
+        redirect('/loginpage')
+    end
+end
+
+post ('/login') do
+    username = params[:username]
+    password = params[:password]
+    db = connect_db()
+    result = db.execute("SELECT * FROM users WHERE username = ?", username).first
+    pwdigest = result["pwdigest"]
+    id = result["id"]
+    if BCrypt::Password.new(pwdigest) == password
+        session[:id] = id
+        redirect('/')
+    else
+        "FEL LÖSENORD"
+    end
+end
+
 get ('/inventory') do
     slim(:inventory)
 end
